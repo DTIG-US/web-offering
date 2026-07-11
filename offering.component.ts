f51279978543a34f9919986656a72e9ae9bfab11
John Aziz <john.aziz@dtig.us>
2026-07-11 18:37:48 -0500

    [Fixed] Offering rendering issue

--- offering.component.ts
+++ offering.component.ts
@@ -1,10 +1,14 @@
 import { Component } from '@angular/core';
+import { CommonModule } from '@angular/common';
+import * as siteData from '../../data.json';
 
 @Component({
   selector: 'app-offering',
+  standalone: true,
+  imports: [CommonModule],
   templateUrl: './offering.component.html',
   styleUrls: ['./offering.component.css']
 })
 export class OfferingComponent {
-
+  data: any = (siteData as any).default;
 }

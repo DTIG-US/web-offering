a44691549adf686e5002f85c013841bc3439a898
John Aziz <john.aziz@dtig.us>
2026-08-06 17:05:02 -0500

    Standardized theme across css

--- offering.component.ts
+++ offering.component.ts
@@ -1,11 +1,9 @@
 import { Component } from '@angular/core';
-import { CommonModule } from '@angular/common';
 import * as siteData from '../../data.json';
 
 @Component({
   selector: 'app-offering',
-  standalone: true,
-  imports: [CommonModule],
+  imports: [],
   templateUrl: './offering.component.html',
   styleUrls: ['./offering.component.css']
 })
